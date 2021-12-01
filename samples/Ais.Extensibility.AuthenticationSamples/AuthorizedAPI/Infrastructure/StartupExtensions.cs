30b4a04646a2507b23d206817ef295286051b68d
Giedrius Gercmanas <17136896+ggercman@users.noreply.github.com>
2021-12-01 15:04:46 +0200

    Add project files.

---
+++ samples/Ais.Extensibility.AuthenticationSamples/AuthorizedAPI/Infrastructure/StartupExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace AuthorizedAPI.Infrastructure
+{
+    internal static class StartupExtensions
+    {
+        public static IServiceCollection AddAppAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    configuration.Bind("JwtBearer", options);
+                });
+            return services;
+        }
+    }
+}
---
+++ samples/Ais.Extensibility.AuthenticationSamples/ClientApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using IdentityModel.Client;
+
+Console.WriteLine("Wait for AIS to start");
+Console.WriteLine("Wait for AuthorizedApi to start");
+Console.WriteLine("Update server URL if AIS Identity runs on another url (https://localhost:6001/connect/token)");
+Console.WriteLine("Press any key to continue!");
+Console.ReadKey();
+
+
+Console.WriteLine("Enter client_id: ");
+var clientId = Console.ReadLine(); //  "client_id";
+
+Console.WriteLine("Enter client_secret: ");
+var clientSecret = Console.ReadLine(); // "client_secret";
+
+Console.WriteLine("Enter username: ");
+var username = Console.ReadLine(); // "username";
+
+Console.WriteLine("Enter password: ");
+var password = Console.ReadLine(); // "password"; ;
+
+
+
+using var request = new PasswordTokenRequest
+{
+    Address = "https://localhost:6001/connect/token",
+
+    ClientId = clientId, // < client_id
+    ClientSecret = clientSecret, // < client_secret
+
+    UserName = username,
+    Password = password,
+
+    Scope = "api1",
+};
+
+using var httpClient = new HttpClient();
+var tokenResponse = await httpClient.RequestPasswordTokenAsync(request);
+
+if (tokenResponse.IsError)
+{
+    throw new Exception(tokenResponse.Error);
+}
+
+
+
+httpClient.SetBearerToken(tokenResponse.AccessToken);
+
+using var response = await httpClient.GetAsync("https://localhost:7123/WeatherForecast").ConfigureAwait(false);
+
+Console.WriteLine();
+if (response.StatusCode != System.Net.HttpStatusCode.OK)
+{
+    Console.WriteLine("Failure");
+    Console.WriteLine($"Request failed with status={(int)response.StatusCode} ({response.StatusCode})");
+}
+else
+{
+    var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+    Console.WriteLine("Success, results:");
+    Console.WriteLine(result);
+}
+Console.WriteLine();
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
